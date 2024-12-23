49f523dc7103b42878bbd119f1a4056bae381a8d
abenezer <54418157+Abenezer-Daniel-W@users.noreply.github.com>
2024-12-22 22:49:54 -0700

    done with most of the sections

--- src/components/LoginHero.tsx
+++ src/components/LoginHero.tsx
@@ -8,13 +8,13 @@ const LoginHero = () => {
   return (
     <div className="w-full min-h-screen bg-myrtle-green flex items-center justify-center py-28 px-16">
       {/* white box */}
-      <div className="w-screen flex h-fit border-2 border-white items-center justify-center ">
-        <div className="flex-col flex-wrap p-12 w-full h-full w-[50%]">
+      <div className="w-screen flex h-fit border-2 border-white items-center justify-center max-w-7xl ">
+        <div className="flex-col flex-wrap p-12 h-full w-full max-w-6xl">
           <div className="flex flex-wrap mb-5">
             <h1 className="text-6xl font-bold mb-4 text-white">
               Welcome Back! Please Log In
             </h1>
-            <p className="text-white text- leading-7 ">
+            <p className="text-white  leading-7 ">
               Access your account to connect with potential employers and find
               the perfect job.
             </p>
@@ -35,7 +35,7 @@ const LoginHero = () => {
             </button>
           </form>
         </div>
-        <div className="w-full h-full p-3 items-center hidden md:flex">
+        <div className="w-full h-full p-3 items-center hidden md:flex max-w-6xl">
           <img
             className="w-full h-full"
             src={require("../assets/img/Welcome_aboard.png")}
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,16 +1,18 @@
-import React from 'react'
-import TopBar from '../components/TopBar.tsx'
-import LoginHero from '../components/LoginHero.tsx'
+import React from "react";
+import TopBar from "../components/TopBar.tsx";
+import Footer from "../components/Footer.tsx";
+import LoginHero from "../components/LoginHero.tsx";
+import FAQ from "../components/FAQ.tsx";
 
 const Login = () => {
-    return(
-        <>
-            <TopBar/>
-            <LoginHero/>
-            <LoginHero/>
+  return (
+    <>
+      <TopBar />
+      <LoginHero />
+      <FAQ />
+      <Footer />
+    </>
+  );
+};
 
-        </>
-    )
-}
-
-export default Login
+export default Login;

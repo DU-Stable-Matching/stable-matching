5b746584c328cb2d19464cf4bfac8d1b716219a8
Ashley Alfaro <ashley.alfaro@du.edu>
2025-05-17 14:07:02 -0600

    updated get_preferences

--- backend/api/utlils.py
+++ backend/api/utlils.py
@@ -1,6 +1,7 @@
 from .database import SessionLocal
 from .models import Admin, Applicant, Building
 from typing import List, Tuple
+import random
 from fastapi import HTTPException
 from sqlalchemy.orm import Session
 
@@ -66,3 +67,47 @@ def get_preferences():
     #     user_pref =  user_pref[: len(admin_pref)]
 
     return user_pref, admin_pref
+
+
+def get_two_preferences():
+    """
+    Returns two lists of preferences:
+      1. Admin preferences as (admin_id, [applicant_id, …])
+      2. Applicant preferences as (applicant_id, [(building_name, boss_id), …])
+    Raises 404 if no admins or no applicants exist.
+    """
+    db = SessionLocal()
+
+    admins = db.query(Admin).all()
+
+    if not admins:
+        raise Exception("No admins found")
+
+    admin_pref = []
+
+    for ad in admins:
+        ra_needed = ad.building.ra_needed
+        for i in range(ra_needed):
+            temp = ((ad.id,i+1), [])
+            for rank in ad.rankings:
+                applicant_ranking = random.shuffle(rank)
+                for app in applicant_ranking:
+                    temp[1].append(app.applicant_id) 
+        admin_pref.append(temp)
+
+    user_pref = []
+
+    apps = db.query(Applicant).all()
+    if not apps:
+        raise Exception("No users found")
+
+    for app in apps:
+        temp = (app.id, [])
+        app.preferences.sort(key=lambda x: x.rank)
+        for pref in app.preferences:
+            building =  db.query(Building).filter(Building.name == pref.building_name).first()
+            temp[1].append(building.id)
+        user_pref.append(temp)
+
+    return user_pref, admin_pref
+
